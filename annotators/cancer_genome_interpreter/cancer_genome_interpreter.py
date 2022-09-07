3ab45e78483fa3946ad70d814f0e0fcd222a0da0
mlarsen2 <mlarsen@insilico.us.com>
2022-09-07 11:39:35 -0400

    cgi fix and new widget

--- annotators/cancer_genome_interpreter/cancer_genome_interpreter.py
+++ annotators/cancer_genome_interpreter/cancer_genome_interpreter.py
@@ -9,9 +9,29 @@ class CravatAnnotator(BaseAnnotator):
         q = 'select association, biomarker, drug, evidence, source, tumor from cgi where chrom = "{chrom}" and pos = {pos} and ref = "{ref}" and alt = "{alt}"'.format(
             chrom = input_data["chrom"], pos = int(input_data["pos"]), ref = input_data["ref_base"], alt = input_data["alt_base"])
         self.cursor.execute(q)
-        row = self.cursor.fetchone()
-        if row:
-            return {'association': row[0], 'biomarker': row[1], 'drug': row[2], 'evidence': row[3], 'source': row[4], 'tumor': row[5]}
+        rows = self.cursor.fetchall()
+        if rows is not None:
+            all_results = []
+            for row in rows:
+                assoc = row[0]
+                biomarker = row[1]
+                drug = row[2]
+                evidence = row[3]
+                source = row[4]
+                tumor = row[5]
+                full_result = [assoc, biomarker, drug, evidence, source, tumor]
+                all_results.append({'association': assoc, 'biomarker': biomarker, 'drug': drug, 'evidence': evidence, 'source': source, 'tumor': tumor, 'full_result': full_result})
+
+        if all_results:
+            all_results_list = [x['full_result'] for x in all_results]
+            best_mapping = all_results[0]
+            assoc = best_mapping["association"]
+            biomarker = best_mapping["biomarker"]
+            drug = best_mapping["drug"]
+            evidence = best_mapping["evidence"]
+            source = best_mapping["source"]
+            tumor = best_mapping["tumor"]
+            return {'association': assoc, 'biomarker': biomarker, 'drug': drug, 'evidence': evidence, 'source': source, 'tumor': tumor, 'all': all_results_list}
 
     def cleanup(self):
         pass
