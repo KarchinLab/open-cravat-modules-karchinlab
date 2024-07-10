beea961bf12905888a03a1b1809897b5845b3f9e
Kyle Anderson <kanderson@potomacitgroup.com>
2024-07-10 13:53:57 -0400

    Fix query and yml for st judes annotation

--- annotators/st_judes_fusion/st_judes_fusion.py
+++ annotators/st_judes_fusion/st_judes_fusion.py
@@ -4,19 +4,46 @@ from cravat import InvalidData
 import sqlite3
 import os
 
+query = '''SELECT
+        patient,
+        sample,
+        supporting_reads_count,
+        breakpoint_pubmed_id
+    FROM st_judes_fusion
+    WHERE l_chrom = ? AND l_pos = ? AND l_strand = ? and r_chrom = ? and r_pos = ? and r_strand = ?
+'''
+
 class CravatAnnotator(BaseAnnotator):
 
-    def setup(self): 
-        pass
-    
     def annotate(self, input_data, secondary_data=None):
         out = {}
-        out['placeholder_annotation'] = 'placeholder value'
+        l_chrom = input_data['left_chrom']
+        l_pos = input_data['left_break_pos']
+        l_strand = input_data['left_break_strand']
+        r_chrom = input_data['right_chrom']
+        r_pos = input_data['right_break_pos']
+        r_strand = input_data['right_break_strand']
+        params = (l_chrom, l_pos, l_strand, r_chrom, r_pos, r_strand)
+        patients = []
+        samples = []
+        supporting_read_counts = []
+        pubmed_ids = []
+        for (patient, sample, supporting_reads_count, breakpoint_pubmed_id) in self.cursor.execute(query, params):
+            patients.append(patient)
+            samples.append(sample)
+            supporting_read_counts.append(supporting_reads_count)
+            pubmed_ids.append(breakpoint_pubmed_id)
+
+        out['patients'] = patients
+        out['samples'] = samples
+        out['supporting_read_counts'] = supporting_read_counts
+        out['pubmed_ids'] = pubmed_ids
         return out
-    
+
     def cleanup(self):
         pass
-        
+
+
 if __name__ == '__main__':
     annotator = CravatAnnotator(sys.argv)
     annotator.run()
