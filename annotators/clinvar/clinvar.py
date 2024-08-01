159f2a950c9ce1f1b5a9138fe3796713c75b8484
skanderson <124068392+skanderson@users.noreply.github.com>
2024-08-01 14:48:44 -0400

    Feat/clinvar update (#28)
    * Clinvar update with all fields from the vcf
    
    * Add entry to endpoint to highlight a variant with somatic annotations
    * Update test key
    
    * Add germline_or_somatic column and smartfilter

--- annotators/clinvar/clinvar.py
+++ annotators/clinvar/clinvar.py
@@ -4,6 +4,48 @@ from cravat import BaseAnnotator
 from cravat import InvalidData
 import sqlite3
 
+col_order = [
+    'af_go_esp',
+    'af_exac',
+    'af_tgp',
+    'clinvar_allele_id',
+    'disease_names',
+    'clinvar_preferred_names',
+    'disease_refs',
+    'disease_refs_incl',
+    'hgvs',
+    'rev_stat',
+    'sig',
+    'sig_conf',
+    'sig_incl',
+    'variant_type',
+    'variant_type_sequence_ontology',
+    'variant_clinical_sources',
+    'dbvar_id',
+    'clinvar_gene_info',
+    'molecular_consequences',
+    'onc_disease_name',
+    'onc_disease_name_incl',
+    'onc_disease_refs',
+    'onc_disease_refs_incl',
+    'onc_classification',
+    'onc_classification_type',
+    'onc_rev_stat',
+    'onc_classification_conflicting',
+    'allele_origin',
+    'dbsnp_id',
+    'somatic_disease_name',
+    'somatic_disease_name_incl',
+    'somatic_refs',
+    'somatic_refs_incl',
+    'somatic_rev_stat',
+    'somatic_impact',
+    'somatic_impact_incl',
+    'germline_or_somatic'
+]
+info_cols = ', '.join([f'{x}' for x in col_order[:]])
+
+
 class CravatAnnotator(BaseAnnotator):
 
     def setup(self):
@@ -16,19 +58,15 @@ class CravatAnnotator(BaseAnnotator):
     def annotate(self, input_data):
         chrom = input_data['chrom']
         self.cursor.execute(
-            f'select sig, disease_refs, disease_names, rev_stat, id, sig_conf from {chrom} where pos=? and ref=? and alt=?;',
+            f'select {info_cols} from {chrom} where pos=? and ref=? and alt=?;',
             (input_data['pos'], input_data['ref_base'], input_data['alt_base'])
         )
         qr = self.cursor.fetchone()
         if qr is not None:
-            return {
-                'sig':qr[0],
-                'disease_refs':qr[1],
-                'disease_names':qr[2],
-                'rev_stat':qr[3],
-                'id': qr[4],
-                'sig_conf':qr[5]
+            data = {
+                col_order[i]: qr[i] for i in range(0, len(col_order))
             }
+            return data
 
         
 if __name__ == '__main__':
