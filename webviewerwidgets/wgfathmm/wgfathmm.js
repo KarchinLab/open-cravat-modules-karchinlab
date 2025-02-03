b1b1f700b80207bd4b205ff76c52cfe76e267050
Kyle Anderson <kanderson@potomacitgroup.com>
2025-02-03 12:55:58 -0500

    Update fathm widget with two columns for bp4 / pp3
    * Fix variable names
    * Update background color per column

--- webviewerwidgets/wgfathmm/wgfathmm.js
+++ webviewerwidgets/wgfathmm/wgfathmm.js
@@ -18,9 +18,9 @@ widgetGenerators['fathmm'] = {
 			var pidls = pid != null ? pid.split(';') : [];
 			var score = getWidgetData(tabName, 'fathmm', row, 'fathmm_score');
 			var scorels = score != null ? score.split(';') : [];
-			var pathogenic = getWidgetData(tabName, 'fathmm', row, 'bp4_pathogenic');
+			var pathogenic = getWidgetData(tabName, 'fathmm', row, 'pp3_pathogenic');
 			var pathogenicls = pathogenic != null ? pathogenic.split(';') : [];
-			var benign = getWidgetData(tabName, 'fathmm', row, 'pp3_benign');
+			var benign = getWidgetData(tabName, 'fathmm', row, 'bp4_benign');
 			var benignls = benign != null ? benign.split(';') : [];
 			for (var i=0;i<scorels.length;i++){
 				if (scorels[i] == '.'){
@@ -36,7 +36,7 @@ widgetGenerators['fathmm'] = {
 			}
 			var table = getWidgetTableFrame();
 			addEl(div, table);
-			var thead = getWidgetTableHead(['Transcript', 'Protein', 'Score', 'Prediction', 'ACMG/AMP Pathogenicity'],['25%','22%','10%','12%', '30%']);
+			var thead = getWidgetTableHead(['Transcript', 'Protein', 'Score', 'Prediction', 'ACMG/AMP Benignity', 'ACMG/AMP Pathogenicity'],['25%','22%','10%','12%', '15%', '15%']);
 			addEl(table, thead);
 			var tbody = getEl('tbody');
 			addEl(table, tbody);
@@ -46,20 +46,22 @@ widgetGenerators['fathmm'] = {
 				var sitr = scorels[i];
 				var pitr = predls[i];
 				var bp4 = benignls[i]
-				var pp3 = pathogenicls[i]
-				let pathogenicityr = null
-				if (bp4 !== null) {
-					pathogenicity = "BP4 " + bp4
-				} else if (pp3 !== null) {
-					pathogenicity = "PP3 " + pp3
-				} else if (pp3 === null && bp4 === null && vert_score !== null) {
-					pathogenicity = "Indeterminate"
+				var pp3 = pathogenicls[i];
+				if (bp4) {
+					bp4 = "BP4 " + bp4;
+				} else if (pp3) {
+					pp3 = "PP3 " + pp3;
+				} 
+				var tr = getWidgetTableTr([tiditr, piditr, sitr, pitr, bp4, pp3]);
+				const bp4Color = getCalibrationGradientColor(bp4);
+				if (bp4) {
+					$(tr).children().eq(4).css("background-color", bp4Color);
+					$(tr).children().eq(4).css("color", bp4.includes("Strong") ? "white" : "black");
 				}
-				var tr = getWidgetTableTr([tiditr, piditr, sitr, pitr, pathogenicityr]);
-				const color = getCalibrationGradientColor(pathogenicityr)
-				if (pathogenicity !== "") {
-					$(tr).children().eq(4).css("background-color", color);
-					$(tr).children().eq(4).css("color", pathogenicityr.includes("Strong") ? "white" : "black");
+				const pp3Color = getCalibrationGradientColor(pp3);
+				if (pp3) {
+					$(tr).children().eq(5).css("background-color", pp3Color);
+					$(tr).children().eq(5).css("color", pp3.includes("Strong") ? "white" : "black");
 				}
 				addEl(tbody, tr);
 			}
