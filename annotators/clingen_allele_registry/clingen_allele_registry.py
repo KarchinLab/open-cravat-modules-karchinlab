572b4d1c94070ecc330fa31d25295fee15496a68
skanderson <124068392+skanderson@users.noreply.github.com>
2025-02-14 14:27:22 -0500

    Update ClingenAlleleRegistry Annotator to use the allele registry API (#40)

--- annotators/clingen_allele_registry/clingen_allele_registry.py
+++ annotators/clingen_allele_registry/clingen_allele_registry.py
@@ -1,39 +1,32 @@
 import sys
+import urllib.parse
+
+import requests
 from cravat import BaseAnnotator
 
 class CravatAnnotator(BaseAnnotator):
 
     def annotate(self, input_data, secondary_data=None):
+        ALLELE_REGISTRY_ENDPOINT = self.conf['api_url']
         out = {}
-        q = '''SELECT
-                allele_registry_id,
-                disease,
-                mode_of_inheritance,
-                assertion,
-                evidence_codes,
-                summary_of_interpretation,
-                chrom,
-                pos,
-                ref,
-                alt
-            FROM allele_registry
-            WHERE chrom = ? AND pos = ? AND ref = ? AND alt = ?
-        '''
         chrom = input_data['chrom']
         pos = input_data['pos']
         ref = input_data['ref_base']
         alt = input_data['alt_base']
-        params = (chrom, pos, ref, alt)
-        self.cursor.execute(q, params)
-        row = self.cursor.fetchone()
-        if row:
+        transcript = input_data.get('transcript')
+        cchange = input_data.get('cchange')
+        hgvs = f'{transcript}:{cchange}'
+        resp = requests.get(f'{ALLELE_REGISTRY_ENDPOINT}{hgvs}')
+        resp.raise_for_status()
+        result = resp.json()
+
+        if result:
+            full_id = result.get('@id')
+            ca_id = str.split(full_id, '/')[-1]
+            if ca_id and not ca_id.startswith('CA'):
+                ca_id = ''
             out = {
-                'allele_registry_id': row[0],
-                'disease': row[1],
-                'mode_of_inheritance': row[2],
-                'assertion': row[3],
-                'evidence_codes': row[4],
-                'summary_of_interpretation': row[5]
+                'allele_registry_id': ca_id
             }
         return out
 
