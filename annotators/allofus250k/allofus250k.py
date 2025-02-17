ba249e3f4132551f56915b8e1c708ea0c599e761
MoezDawood <dawood.moez@gmail.com>
2025-02-17 11:37:32 -0600

    All of Us 250k v7.1 and Regeneron Million Exome for annotators

---
+++ annotators/allofus250k/allofus250k.py
@@ -0,0 +1,128 @@
+import sys
+from cravat import BaseAnnotator
+from cravat import InvalidData
+import sqlite3
+import os
+
+class CravatAnnotator(BaseAnnotator):
+
+    def setup(self): 
+        """
+        Set up data sources. 
+        Cravat will automatically make a connection to 
+        data/example_annotator.sqlite using the sqlite3 python module. The 
+        sqlite3.Connection object is stored as self.dbconn, and the 
+        sqlite3.Cursor object is stored as self.cursor.
+        """
+        pass
+    
+    def annotate(self, input_data, secondary_data=None):
+        """
+        The annotator parent class will call annotate for each line of the 
+        input file. It takes one positional argument, input_data, and one
+        keyword argument, secondary_data.
+        
+        input_data is a dictionary containing the data from the current input 
+        line. The keys depend on what what file is used as the input, which can 
+        be changed in the module_name.yml file. 
+        Variant level includes the following keys: 
+            ('uid', 'chrom', 'pos', 'ref_base', 'alt_base')
+        Variant level crx files expand the key set to include:
+            ('hugo', 'transcript','so','all_mappings')
+        Gene level files include
+            ('hugo', 'num_variants', 'so', 'all_so')
+        
+        secondary_data is used to allow an annotator to access the output of
+        other annotators. It is described in more detail in the CRAVAT 
+        documentation.
+        
+        annotate should return a dictionary with keys matching the column names
+        defined in example_annotator.yml. Extra column names will be ignored, 
+        and absent column names will be filled with None. Check your output
+        carefully to ensure that your data is ending up where you intend.
+        """
+        chrom = input_data['chrom']
+        pos = input_data['pos']
+        ref_base = input_data['ref_base']
+        alt_base = input_data['alt_base']
+        
+        query = f'select gvs_all_ac, gvs_all_an, gvs_all_af, gvs_max_af, gvs_max_ac, gvs_max_an, gvs_max_subpop, gvs_afr_ac, gvs_afr_an, gvs_afr_af, gvs_amr_ac, gvs_amr_an, gvs_amr_af, gvs_eas_ac, gvs_eas_an, gvs_eas_af, gvs_eur_ac, gvs_eur_an, gvs_eur_af, gvs_mid_ac, gvs_mid_an, gvs_mid_af, gvs_oth_ac, gvs_oth_an, gvs_oth_af, gvs_sas_ac, gvs_sas_an, gvs_sas_af from allofus250k where chrom="{chrom}" and pos={pos} and ref="{ref_base}" and alt="{alt_base}";'
+        self.cursor.execute(query)
+        result = self.cursor.fetchone()
+        if result is not None:
+            gvs_all_ac = result[0]
+            gvs_all_an = result[1]
+            gvs_all_af = result[2]
+            gvs_max_af = result[3]
+            gvs_max_ac = result[4]
+            gvs_max_an = result[5]
+            gvs_max_subpop = result[6]
+            gvs_afr_ac = result[7]
+            gvs_afr_an = result[8]
+            gvs_afr_af = result[9]
+            gvs_amr_ac = result[10]
+            gvs_amr_an = result[11]
+            gvs_amr_af = result[12]
+            gvs_eas_ac = result[13]
+            gvs_eas_an = result[14]
+            gvs_eas_af = result[15]
+            gvs_eur_ac = result[16]
+            gvs_eur_an = result[17]
+            gvs_eur_af = result[18]
+            gvs_mid_ac = result[19]
+            gvs_mid_an = result[20]
+            gvs_mid_af = result[21]
+            gvs_oth_ac = result[22]
+            gvs_oth_an = result[23]
+            gvs_oth_af = result[24]
+            gvs_sas_ac = result[25]
+            gvs_sas_an = result[26]
+            gvs_sas_af = result[27]
+
+            return {
+                'gvs_all_ac': gvs_all_ac,
+                'gvs_all_an': gvs_all_an,
+                'gvs_all_af': gvs_all_af,
+                'gvs_max_af': gvs_max_af,
+                'gvs_max_ac': gvs_max_ac,
+                'gvs_max_an': gvs_max_an,
+                'gvs_max_subpop': gvs_max_subpop,
+                'gvs_afr_ac': gvs_afr_ac,
+                'gvs_afr_an': gvs_afr_an,
+                'gvs_afr_af': gvs_afr_af,
+                'gvs_amr_ac': gvs_amr_ac,
+                'gvs_amr_an': gvs_amr_an,
+                'gvs_amr_af': gvs_amr_af,
+                'gvs_eas_ac': gvs_eas_ac,
+                'gvs_eas_an': gvs_eas_an,
+                'gvs_eas_af': gvs_eas_af,
+                'gvs_eur_ac': gvs_eur_ac,
+                'gvs_eur_an': gvs_eur_an,
+                'gvs_eur_af': gvs_eur_af,
+                'gvs_mid_ac': gvs_mid_ac,
+                'gvs_mid_an': gvs_mid_an,
+                'gvs_mid_af': gvs_mid_af,
+                'gvs_oth_ac': gvs_oth_ac,
+                'gvs_oth_an': gvs_oth_an,
+                'gvs_oth_af': gvs_oth_af,
+                'gvs_sas_ac': gvs_sas_ac,
+                'gvs_sas_an': gvs_sas_an,
+                'gvs_sas_af': gvs_sas_af}
+        else:
+            return None
+
+        out = {}
+        out['placeholder_annotation'] = 'placeholder value'
+        return out
+    
+    def cleanup(self):
+        """
+        cleanup is called after every input line has been processed. Use it to
+        close database connections and file handlers. Automatically opened
+        database connections are also automatically closed.
+        """
+        pass
+        
+if __name__ == '__main__':
+    annotator = CravatAnnotator(sys.argv)
+    annotator.run()
