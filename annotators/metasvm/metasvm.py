1d79cdd0f46944620fceabc9359459f918479e9e
Charlie Vaske <charlie@seavaske.com>
2025-03-23 13:56:33 -0700

    metasvm: add VEP calibration

--- annotators/metasvm/metasvm.py
+++ annotators/metasvm/metasvm.py
@@ -1,8 +1,38 @@
+
 import sys
 from cravat import BaseAnnotator
 from cravat import InvalidData
 import sqlite3
 import os
+
+
+def discretize_scalar(score, cutoffs):
+    """Locate the location of `score` in a list[tuple(float, str)] of
+    `cutoffs`, where the float cutoff is the maximum value, inclusive
+    of the value, for that label. The last tuple should typically have
+    `float("inf")` as the cutoff, otherwise the function may retun
+    `None`
+
+    The cutoffs must be sorted in increasing value.
+    """
+    prev_cutoff = None
+    for cutoff, label in cutoffs:
+        if score <= cutoff:
+            return label
+        if prev_cutoff is not None and prev_cutoff > cutoff:
+            raise ValueError("cutoffs are not sorted")
+        prev_cutoff = cutoff
+
+
+BP4_CUTOFFS = [
+    (float("inf"), "")
+]
+
+PP3_CUTOFFS = [
+    (0.5804, ""),
+    (0.8573, "Supporting"),
+    (float("inf"), "Moderate")
+]
 
 class CravatAnnotator(BaseAnnotator):
     
@@ -18,7 +48,13 @@ class CravatAnnotator(BaseAnnotator):
                 pred = 'Damaging'
             else:
                 pred = None
-            out = {'score': row[0], 'rankscore': row[1], 'pred': pred}
+            out = {
+                'score': row[0],
+                'rankscore': row[1],
+                'pred': pred,
+                "bp4_benign": discretize_scalar(row[0], BP4_CUTOFFS),
+                "pp3_pathogenic": discretize_scalar(row[0], PP3_CUTOFFS),
+            }
             return out
     
     def cleanup(self):
