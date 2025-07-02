0ace5e8a18ab8a2c86b2caf133e5a8818ad6ea3f
Kyle Anderson <kanderson@potomacitgroup.com>
2025-07-02 16:35:53 -0400

    Update GWAS Catalog with new data
    * Add All Annotations column to return all GWAS hits for a given chrom /
      pos
    * Add Risk Allele Match column to indicate if the highest risk allele
      from any GWAS study matches the input variant

--- annotators/gwas_catalog/gwas_catalog.py
+++ annotators/gwas_catalog/gwas_catalog.py
@@ -10,6 +10,7 @@ class CravatAnnotator(BaseAnnotator):
         pass
     
     def annotate(self, input_data):
+        main = {}
         out = {}
         cols = [
             'disease',
@@ -27,9 +28,24 @@ class CravatAnnotator(BaseAnnotator):
             input_data['pos']
         )
         self.cursor.execute(q)
-        qr = self.cursor.fetchone()
-        if qr is not None:
-            out = dict(zip(cols, qr))
+        if self.cursor.rowcount:
+            first_row = next(self.cursor)
+            main = dict(zip(cols, first_row))
+            match = main['risk_allele'] == input_data['ref_base'] or main['risk_allele'] == input_data['alt_base']
+            main['risk_allele_match'] = match
+            out = main.copy()
+            all_anots = []
+            all_anots.append(main)
+            has_match = match
+            for qr in self.cursor:
+                line_data = dict(zip(cols, qr))
+                match = line_data['risk_allele'] == input_data['ref_base'] or line_data['risk_allele'] == input_data['alt_base']
+                line_data['risk_allele_match'] = match
+                has_match = has_match or match
+                all_anots.append(line_data)
+            out['risk_allele_match'] = has_match
+            out['all_annots'] = all_anots
+
         return out
     
     def cleanup(self):
