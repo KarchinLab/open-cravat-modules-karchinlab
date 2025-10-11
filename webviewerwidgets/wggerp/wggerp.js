b6ee0c31edd10488281e7a9e74d7b0dcaaa03205
Charlie Vaske <charlie@seavaske.com>
2025-10-11 11:16:02 -0700

    Fix Gerp widget and annotater
    * Remove logging line from annotater
    * Change webviewerwidget to a table row rather than info lines

--- webviewerwidgets/wggerp/wggerp.js
+++ webviewerwidgets/wggerp/wggerp.js
@@ -4,20 +4,38 @@ widgetGenerators['gerp'] = {
 		'height': 120, 
 		'default_hidden': false,
 		'function': function (div, row, tabName) {
-			addInfoLine(div, row, 'Neutral Rate', 'gerp__gerp_nr', tabName);
-			addInfoLine(div, row, 'RS Score', 'gerp__gerp_rs', tabName);
-			addInfoLine(div, row, 'RS Ranked Score', 'gerp__gerp_rs_rank', tabName);
-			if (score != undefined) {
-				var pathogenicity = "Indeterminate";
-				var bp4 = getWidgetData(tabName, 'gerp', row, 'bp4_benign');
-				var pp3 = getWidgetData(tabName, 'gerp', row, 'pp3_pathogenicity');
-				if (bp4) {
-					pathogenic = "BP4 " + bp4;
-				} else {
-					pathogenic = "PP3 " + pp3;
-				}
-				addInfoLineText(div, 'ACMG/AMP Pathogenicity', pathogenic)
-			} 
+			let rank_score = getWidgetData(tabName, 'gerp', row, 'gerp_rs_rank')
+			if (rank_score === undefined) {
+				return;
+			}
+			addInfoLine(div, row, 'Gerp Neutral Rate', 'gerp__gerp_nr', tabName);
+
+			let neutral_rate = getWidgetData(tabName, 'gerp', row,' gerp_nr')
+			let rs_score = getWidgetData(tabName, 'gerp', row, 'gerp_rs')
+			var pathogenic = "Indeterminate";
+			var bp4 = getWidgetData(tabName, 'gerp', row, 'bp4_benign');
+			var pp3 = getWidgetData(tabName, 'gerp', row, 'pp3_pathogenicity');
+			if (bp4) {
+				pathogenic = "BP4 " + bp4;
+			} else {
+				pathogenic = "PP3 " + pp3;
+			}
+			var table = getWidgetTableFrame();
+            var thead = getWidgetTableHead([
+				'Neutral Rate', 
+				'RS Score', 
+				'RS Rank Score', 
+				'ACMG/AMP Pathogenicity'
+			]);
+			var tr = getWidgetTableTr([neutral_rate, rs_score, rank_score, pathogenic]);
+			const color = getCalibrationGradientColor(pathogenic);
+			if (pathogenic) {
+				$(tr).children().eq(3).css("background-color", color);
+				$(tr).children().eq(3).css("color", pathogenic.includes("Strong") ? "white" : "black");
+			}
+			addEl(div, table);
+			addEl(table, thead);
+			addEl(table, tr);
 		}
 	}
 }
